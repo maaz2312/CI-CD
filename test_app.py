670045f0b90a0a9276ac3e15ece8f1967a4f5bcc
maaz2312 <maaazmuhammad35@gmail.com>
2025-03-21 10:30:29 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pytest
+from main import subtract  # Import function from main.py
+
 def test_subtract():
     assert subtract(10, 5) == 5
     assert subtract(0, 5) == -5
     assert subtract(-5, -5) == 0
     assert subtract(5, 10) == -5
-
-if __name__ == "__main__":
-    pytest.main()
-
